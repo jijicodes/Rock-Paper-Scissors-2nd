241d90b92ca712c371f83b801eea8f7e9e96a770
Jenna <sjieun11@gmail.com>
2021-05-23 17:26:05 -0600

    refactored rockplay, scissorplay, and paper play functions

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,25 +1,53 @@
+/**
+ * @typedef {"✊" | "✋" | "✌"} SelectionOption
+ * @typedef {[number, number, number]} Score
+ */
+
+/**
+ * @returns {SelectionOption}
+ */
 function computerPlay() {
   return items[Math.floor(Math.random() * items.length)];
 }
 
+/**
+ * @type {[SelectionOption,SelectionOption, SelectionOption]}
+ */
 let items = ["✊", "✋", "✌"];
 
+/**
+ * @type {Score}
+ */
 let emptyScore = [0, 0, 0];
 
+/**
+ *
+ * @param {SelectionOption} userPick
+ * @param {SelectionOption} computerPick
+ * @returns {void}
+ */
 function updateSelections(userPick, computerPick) {
   const userSelect = document.querySelector("#userSelect");
   const computerSelect = document.querySelector("#computerSelect");
-  userSelect.textContent = `You picked ${userPick}`;
-  computerSelect.textContent = `Computer picked ${computerPick}`;
+  userSelect && (userSelect.textContent = `You picked ${userPick}`);
+  computerSelect &&
+    (computerSelect.textContent = `Computer picked ${computerPick}`);
 }
 
-function playRound(playerSelection, computerSelection, score) {
-  if (playerSelection == computerSelection) {
+/**
+ *
+ * @param {SelectionOption} userSelection
+ * @param {SelectionOption} computerSelection
+ * @param {Score} score
+ * @returns {Score}
+ */
+function playRound(userSelection, computerSelection, score) {
+  if (userSelection == computerSelection) {
     return [score[0], score[1], score[2] + 1];
   } else if (
-    (playerSelection == "✌" && computerSelection == "✋") ||
-    (playerSelection == "✋" && computerSelection == "✊") ||
-    (playerSelection == "✊" && computerSelection == "✌")
+    (userSelection == "✌" && computerSelection == "✋") ||
+    (userSelection == "✋" && computerSelection == "✊") ||
+    (userSelection == "✊" && computerSelection == "✌")
   ) {
     return [score[0] + 1, score[1], score[2]];
   } else {
@@ -27,74 +55,66 @@ function playRound(playerSelection, computerSelection, score) {
   }
 }
 
+/**
+ *
+ * @param {Score} score
+ * @returns {boolean}
+ */
 function gameIsOver(score) {
   return score[0] === 3 || score[1] === 3;
 }
 
 function displayGameOver() {
-  document.querySelector("#reset").classList.add("visible");
+  document.querySelector("#reset")?.classList.add("visible");
 }
 
+/**
+ * @type {Score}
+ */
 let currentScore = emptyScore;
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  function playRock() {
-    if (gameIsOver(currentScore)) {
-      return;
-    }
-    const computerPick = computerPlay();
-    const newScore = playRound("✊", computerPick, currentScore);
-    currentScore = newScore;
-    updateSelections("✊", computerPick);
-    displayResults(currentScore);
-    gameIsOver(currentScore) && displayGameOver();
-  }
-
-  function playPaper() {
-    if (gameIsOver(currentScore)) {
-      return;
-    }
-    const computerPick = computerPlay();
-    let newScore = playRound("✋", computerPlay(), currentScore);
-    currentScore = newScore;
-    updateSelections("✋", computerPick);
-    displayResults(currentScore);
-    gameIsOver(currentScore) && displayGameOver();
-  }
-
-  function playScissors() {
-    if (gameIsOver(currentScore)) {
-      return;
-    }
-    const computerPick = computerPlay();
-    let newScore = playRound("✌", computerPlay(), currentScore);
-    currentScore = newScore;
-    updateSelections("✌", computerPick);
-    displayResults(currentScore);
-    gameIsOver(currentScore) && displayGameOver();
-  }
-
   const rock = document.querySelector("#rock button");
   const paper = document.querySelector("#paper button");
   const scissors = document.querySelector("#scissors button");
-  rock.addEventListener("click", playRock);
-  paper.addEventListener("click", playPaper);
-  scissors.addEventListener("click", playScissors);
+  rock?.addEventListener("click", () => rockPaperScissors("✊"));
+  paper?.addEventListener("click", () => rockPaperScissors("✋"));
+  scissors?.addEventListener("click", () => rockPaperScissors("✌"));
 
   const gameOver = document.querySelector("#reset");
-  gameOver.addEventListener("click", function (e) {
+  gameOver?.addEventListener("click", function () {
     currentScore = emptyScore;
     displayResults(currentScore);
     gameOver.classList.remove("visible");
   });
-
-  function displayResults(newScore) {
-    const win = document.querySelector("#win");
-    const loss = document.querySelector("#loss");
-    const tied = document.querySelector("#tied");
-
-    win.textContent = newScore[0];
-    loss.textContent = newScore[1];
-    tied.textContent = newScore[2];
-  }
 });
+
+/**
+ *
+ * @param {Score} newScore
+ */
+function displayResults(newScore) {
+  const win = document.querySelector("#win");
+  const loss = document.querySelector("#loss");
+  const tied = document.querySelector("#tied");
+
+  win && (win.textContent = `WIN ${newScore[0]}`);
+  loss && (loss.textContent = `LOSS ${newScore[1]}`);
+  tied && (tied.textContent = `TIED ${newScore[2]}`);
+}
+
+/**
+ *
+ * @param {SelectionOption} userPick
+ */
+function rockPaperScissors(userPick) {
+  if (gameIsOver(currentScore)) {
+    return;
+  }
+  const computerPick = computerPlay();
+  const newScore = playRound(userPick, computerPick, currentScore);
+  currentScore = newScore;
+  updateSelections(userPick, computerPick);
+  displayResults(currentScore);
+  gameIsOver(currentScore) && displayGameOver();
+}
